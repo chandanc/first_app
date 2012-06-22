950584afd42e8883cd2afae55ecd9acc65a1f5a7
U-Alka-PC\Alka <Alka@Alka-PC.(none)>
2012-06-22 23:39:21 +1200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f973f1a58f3a6596c3bc68b335f023ad8eca81d1f33e08a7336468fa6a20be0e1b661ae1160e97b537e1b7ef4e795c474e4b641feaea2eafa243b36d99456f86'
